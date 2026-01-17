31f249bdb9b5de374ea74be163b104438599647e
Golam Rabbi <golam.rabbi221b@gmail.com>
2026-01-17 21:07:05 +0600

    all the setup and for user in backend all the api was created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return <div>
+      <h1 class="text-3xl font-bold text-orange-600 underline">
+    Hello world!
+  </h1>
+
+  </div>;
+};
+
+export default App;
